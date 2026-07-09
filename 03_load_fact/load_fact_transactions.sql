224d1660af03595ab8c908eccc1cdb394348f869
Hanin Al Mahruqi <72063705+hanin-almahruqi@users.noreply.github.com>
2026-07-09 15:55:21 +0400

    Refactor SQL insert for Fact_Transactions
    Updated SQL insert statement to use new table and aliases for clarity.

--- 03_load_fact/load_fact_transactions.sql
+++ 03_load_fact/load_fact_transactions.sql
@@ -1,4 +1,5 @@
-INSERT INTO Fact_Transactions (
+INSERT INTO Fact_Transactions
+(
     Record_ID,
     Recipient_ID,
     Manufacturer_ID,
@@ -14,53 +15,73 @@ INSERT INTO Fact_Transactions (
     Program_Year
 )
 SELECT
-    o.Record_ID,
-
-    r.Recipient_ID,
-    m.Manufacturer_ID,
-    h.Teaching_Hospital_ID,
-    p.Payment_ID,
-    t.Travel_ID,
-    tp.Third_Party_ID,
-    pr.Product_ID,
-    d1.Date_ID,
-    d2.Date_ID,
-
-    o.Total_Amount_of_Payment_USDollars,
-    o.Number_of_Payments_Included_in_Total_Amount,
-    o.Program_Year
-
-FROM OP_DTL_GNRL_PGYR2024_P01232026 o
-
-LEFT JOIN Dim_Recipient r
-    ON o.Covered_Recipient_Profile_ID = r.Covered_Recipient_Profile_ID
-
-LEFT JOIN Dim_Manufacturer m
-    ON o.Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_ID
-     = m.Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_ID
-
-LEFT JOIN Dim_Teaching_Hospital h
-    ON o.Teaching_Hospital_ID = h.Teaching_Hospital_ID_Source
-
-LEFT JOIN Dim_Payment p
-    ON o.Nature_of_Payment_or_Transfer_of_Value = p.Nature_of_Payment_or_Transfer_of_Value
-   AND o.Form_of_Payment_or_Transfer_of_Value = p.Form_of_Payment_or_Transfer_of_Value
-
-LEFT JOIN Dim_Travel t
-    ON o.City_of_Travel = t.City_of_Travel
-   AND o.State_of_Travel = t.State_of_Travel
-   AND o.Country_of_Travel = t.Country_of_Travel
-
-LEFT JOIN Dim_Third_Party tp
-    ON o.Name_of_Third_Party_Entity_Receiving_Payment_or_Transfer_of_Value
-     = tp.Name_of_Third_Party_Entity_Receiving_Payment_or_Transfer_of_Value
-
-LEFT JOIN Dim_Product pr
-    ON o.Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_1
-     = pr.Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_1
-
-LEFT JOIN Dim_Date d1
-    ON o.Date_of_Payment = d1.Full_Date
-
-LEFT JOIN Dim_Date d2
-    ON o.Payment_Publication_Date = d2.Full_Date;
+    op.Record_ID,
+    dr.Recipient_ID,
+    dm.Manufacturer_ID,
+    dth.Teaching_Hospital_ID,
+    dp.Payment_ID,
+    dt.Travel_ID,
+    dtp.Third_Party_ID,
+    dpr.Product_ID,
+    dd_payment.Date_ID,
+    dd_publication.Date_ID,
+    op.Total_Amount_of_Payment_USDollars,
+    op.Number_of_Payments_Included_in_Total_Amount,
+    op.Program_Year
+FROM OP_DTL_GNRL_PGYR2024_P01232026_2 op
+    
+LEFT JOIN Dim_Recipient dr
+    ON op.Covered_Recipient_Profile_ID =
+       dr.Covered_Recipient_Profile_ID
+    
+LEFT JOIN Dim_Manufacturer dm
+    ON op.Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_ID =
+       dm.Applicable_Manufacturer_or_Applicable_GPO_Making_Payment_ID
+    
+LEFT JOIN Dim_Teaching_Hospital dth
+    ON op.Teaching_Hospital_ID =
+       dth.Teaching_Hospital_ID_Source
+    
+LEFT JOIN Dim_Payment dp
+    ON op.Form_of_Payment_or_Transfer_of_Value =
+       dp.Form_of_Payment_or_Transfer_of_Value
+   AND op.Nature_of_Payment_or_Transfer_of_Value =
+       dp.Nature_of_Payment_or_Transfer_of_Value
+    
+LEFT JOIN Dim_Travel dt
+    ON op.City_of_Travel =
+       dt.City_of_Travel
+   AND op.State_of_Travel =
+       dt.State_of_Travel
+   AND op.Country_of_Travel =
+       dt.Country_of_Travel
+    
+LEFT JOIN Dim_Third_Party dtp
+    ON op.Third_Party_Payment_Recipient_Indicator =
+       dtp.Third_Party_Payment_Recipient_Indicator
+   AND op.Name_of_Third_Party_Entity_Receiving_Payment_or_Transfer_of_Value =
+       dtp.Name_of_Third_Party_Entity_Receiving_Payment_or_Transfer_of_Value
+   AND op.Charity_Indicator =
+       dtp.Charity_Indicator
+   AND op.Third_Party_Equals_Covered_Recipient_Indicator =
+       dtp.Third_Party_Equals_Covered_Recipient_Indicator
+    
+LEFT JOIN Dim_Product dpr
+    ON op.Related_Product_Indicator =
+       dpr.Related_Product_Indicator
+   AND op.Covered_or_Noncovered_Indicator_1 =
+       dpr.Covered_or_Noncovered_Indicator_1
+   AND op.Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1 =
+       dpr.Indicate_Drug_or_Biological_or_Device_or_Medical_Supply_1
+   AND op.Product_Category_or_Therapeutic_Area_1 =
+       dpr.Product_Category_or_Therapeutic_Area_1
+   AND op.Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_1 =
+       dpr.Name_of_Drug_or_Biological_or_Device_or_Medical_Supply_1
+    
+LEFT JOIN Dim_Date dd_payment
+    ON op.Date_of_Payment =
+       dd_payment.Full_Date
+    
+LEFT JOIN Dim_Date dd_publication
+    ON op.Payment_Publication_Date =
+       dd_publication.Full_Date;
